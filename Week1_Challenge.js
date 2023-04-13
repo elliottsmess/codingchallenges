3b6777b8ce86ff93968a54fdbe1f6269c2f4828c
elliottsmess <118229857+elliottsmess@users.noreply.github.com>
2023-04-13 17:44:02 -0500

    Update Week1_Challenge.js

--- Week1_Challenge.js
+++ Week1_Challenge.js
@@ -1,1 +1,8 @@
+function triArea(base, height) {
+  var area = (base * height) / 2;
+  return area;
+}
 
+console.log(triArea(3, 2)); // Output: 3
+console.log(triArea(7, 4)); // Output: 14
+console.log(triArea(10, 10)); // Output: 50
