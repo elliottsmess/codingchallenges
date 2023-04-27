5b39f563c0af5fe868e10d81bf7bf48d31aa8d7b
elliottsmess <118229857+elliottsmess@users.noreply.github.com>
2023-04-27 17:43:08 -0500

    Update Week3_Challenge.js

--- Week3_Challenge.js
+++ Week3_Challenge.js
@@ -1,1 +1,16 @@
+function FirstReverse(str) {
+  // Split the string into an array of characters
+  var charArray = str.split("");
 
+  // Reverse the array of characters
+  var reversedArray = charArray.reverse();
+
+  // Join the array of characters back into a string
+  var reversedString = reversedArray.join("");
+
+  // Return the reversed string
+  return reversedString;
+}
+   
+// keep this function call here 
+console.log(FirstReverse(readline()));
