29a97e8cd81090263372864824ac60450210d8de
elliottsmess <118229857+elliottsmess@users.noreply.github.com>
2023-05-08 17:22:43 -0500

    Update Week4_Challenge.js

--- Week4_Challenge.js
+++ Week4_Challenge.js
@@ -1,1 +1,18 @@
+function LongestWord(sen) { 
 
+  // code goes here  
+  sen = sen.replace(/[^a-zA-Z0-9\s]/g, '');
+  var words = sen.split(' ');
+  var longestWord = words[0];
+
+
+   for (var i = 1; i < words.length; i++) {
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i];
+    }
+  }
+  
+  return longestWord;
+}
+// keep this function call here 
+console.log(LongestWord(readline()));
