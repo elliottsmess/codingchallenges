dec572faae9585ddec629c8cf5b8b25e74c1f8f7
elliottsmess <118229857+elliottsmess@users.noreply.github.com>
2023-06-09 21:54:31 -0500

    completed challenge

--- Week7_Challenge.js
+++ Week7_Challenge.js
@@ -1,1 +1,16 @@
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
+  compareAge(otherPerson) {
+    if (this.age < otherPerson.age) {
+      return `${otherPerson.name} is older than me.`;
+    } else if (this.age > otherPerson.age) {
+      return `${otherPerson.name} is younger than me.`;
+    } else {
+      return `${otherPerson.name} is the same age as me.`;
+    }
+  }
+}
